a09afc80fa49451e769d96769567e638c3778b33
Matt Rowles <matthewrowles@gmail.com>
2017-08-25 17:58:07 +1000

    :|

--- run_server.sh
+++ run_server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export MACQUARIE_CLIENT_ID='aqMchxLQ3sldvvKEZxMBmd95lBhnhm4z'
-export MACQUARIE_CLIENT_SECRET='JNBQkJ22vcU2ynMf'
+export MACQUARIE_CLIENT_ID=
+export MACQUARIE_CLIENT_SECRET=
 
 mvn spring-boot:run -Dmacquarie.oauth.clientId=${MACQUARIE_CLIENT_ID} -Dmacquarie.oauth.clientSecret=${MACQUARIE_CLIENT_SECRET}
